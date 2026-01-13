7ceb3df73672f893f61db9a1c327b3b86cf37027
guasmo <juancalveti4@gmail.com>
2026-01-13 17:52:06 -0500

    feat: initialize nestjs application (src/main.ts)

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setDescription('The API for the application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  logger: ['error', 'warn', 'log', 'debug']
+
+  app.enableCors({
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application running on: http://localhost:${port}`);
+}
+
+bootstrap();
