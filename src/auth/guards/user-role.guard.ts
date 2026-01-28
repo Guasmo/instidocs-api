3e875711316f5154ff0f870bf8b1725d29c8dcff
guasmo <juancalveti4@gmail.com>
2026-01-28 09:17:56 -0500

    feat: add role-based access control decorators and guards

---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,31 @@
+
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { META_ROLES } from "../decorators/role-protected.decorator";
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+
+    constructor(private readonly reflector: Reflector) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const validRoles: string[] = this.reflector.get(META_ROLES, context.getHandler());
+
+        if (!validRoles) return true;
+        if (validRoles.length === 0) return true;
+
+        const req = context.switchToHttp().getRequest();
+        const user = req.user;
+
+        if (!user) throw new BadRequestException('User not found');
+
+        if (validRoles.includes(user.role)) {
+            return true;
+        }
+
+        throw new ForbiddenException(
+            `User ${user.fullName} need a valid role: [${validRoles}]`
+        );
+    }
+}
